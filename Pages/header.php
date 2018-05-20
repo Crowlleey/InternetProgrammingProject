b641a151b23239efd6429a21c25bc73c197d5a7d
George Gomes <georgegomees@gmail.com>
2018-05-19 21:25:46 -0300

    Home page is done(responsive for all devices)

--- Pages/header.php
+++ Pages/header.php
@@ -2,8 +2,10 @@
 <html>
 <head>
     <meta charset="utf-8" />
+    <meta name= viewport content= äwidth=device-width initial-scale=1 ,user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><!--TITLE--></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="Style/default.css?$$REVISION$$">
+
 </head>
--- index.php
+++ index.php
@@ -9,19 +9,52 @@
 
 <body>
 
+     <div class="header"> 
+            <div class="title col-11 col-sm-10 col-md-10 col-lg-8 col-xl-6">
+                <label for="title" id="title">George Gomes</label> <br>
+                <label for="subtitle" id="subtitle">iOS developer</label>
+            </div>
+
+            <div id="continue">
+                <div id="divbtcontinue">
+                    <button id="btcontinue" class=" col-sm-12 col-md-12">CONTINUE</button> 
+                </div> 
+                <i class="arrow down" id="arrowa"></i><br>
+                <i class="arrow down" id="arrowb"></i>
+                
+              
+            </div>
+    </div>
+
+    <nav > 
+        <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">ABOUT ME</a></li>
+            <li><a href="#">SKILLS</a></li>
+            <li><a href="#">PROJECTS</a></li>
+            <li><a href="#">CONTACT</a></li>
+        </ul>                
+    </nav>
+
+
+
+
     <div class="header"> 
-        <div class="title">
-            <label for="title" id="title">George Gomes</label> <br>
-            <label for="subtitle" id="subtitle">iOS developer</label>
-
-
     </div>
 
 
+
+
+    <div class="header"> 
     </div>
+
+
 
 
 </body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 </html>
 
 
